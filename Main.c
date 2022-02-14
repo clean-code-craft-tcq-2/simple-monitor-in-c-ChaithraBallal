69fdd7bc33ed63847382d6ade4fe9e662d5ae236
ChaithraBallal <91113671+ChaithraBallal@users.noreply.github.com>
2022-02-14 23:18:10 +0530

    Update Main.c

--- Main.c
+++ Main.c
@@ -18,5 +18,12 @@ int batteryIsOk(float temperature, float soc, float chargeRate, int (*fptempChec
 int main() 
 {
   assert(batteryIsOk(25, 70, 0.7, &tempCheck, &socCheck, &chargeRateCheck));
-  assert(!batteryIsOk(50, 85, 0, &tempCheck, &socCheck, &chargeRateCheck));
+  assert(!batteryIsOk(-2.25, 70, 0.7, &tempCheck, &socCheck, &chargeRateCheck));
+  assert(!batteryIsOk(42.25, 70, 0.7, &tempCheck, &socCheck, &chargeRateCheck));
+  assert(!batteryIsOk(25, 19, 0.7, &tempCheck, &socCheck, &chargeRateCheck));
+  assert(!batteryIsOk(25, 86, 0.7, &tempCheck, &socCheck, &chargeRateCheck));
+  assert(!batteryIsOk(25, 23, 0.7, &tempCheck, &socCheck, &chargeRateCheck));
+  assert(!batteryIsOk(25, 78, 0.7, &tempCheck, &socCheck, &chargeRateCheck));
+  assert(!batteryIsOk(25, 23, 0,9 &tempCheck, &socCheck, &chargeRateCheck));
+  assert(!batteryIsOk(25, 78, 0.78, &tempCheck, &socCheck, &chargeRateCheck));
 }
