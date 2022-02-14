de7e39fa263f8e85405ff7bc901661c39309f24b
ChaithraBallal <91113671+ChaithraBallal@users.noreply.github.com>
2022-02-14 22:07:56 +0530

    Update RangeCheck.c

--- RangeCheck.c
+++ RangeCheck.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
 #include <assert.h>
+#include <RangeCheck.h>
 
-#define SOCLOWTHRESHOLD               20
-#define SOCHIGHTHRESHOLD              80
-#define SOCLOWTHRESHOLD_WARNING       24
-#define SOCHIGHTHRESHOLD_WARNING      76
-#define TEMPLOWTHRESHOLD              0
-#define TEMPHIGHTHRESHOLD             45
-#define TEMPLOWTHRESHOLD_WARNING     -2.25
-#define TEMPHIGHTHRESHOLD_WARNING     42.25
-#define CHARGERATETHRESHOLD           0.8
-#define CHARGERATETHRESHOLD_WARNING   0.76
+char OutOfRange[100] = "Out Of Range";
+char WarningOutOfRange[100] = "Approaching Out of Range";
+char BMS_Normal[100] = "Battery is normal";
 
 int tempCheck(float temperature)
 {
